980b983d0113903ba5f49ea9649bba6b56f7b52d
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-10-05 21:45:41 +0300

    Handle func added for new room.

--- main.go
+++ main.go
@@ -25,8 +25,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, req *http.Request)  {
 
 func main() {
 
+	r := newRoom()
+
 	// root
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
 
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
