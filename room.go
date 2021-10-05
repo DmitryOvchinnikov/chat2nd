37b020ee900be0427c7bdc0c95dd1a0d3f069832
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-10-05 21:19:23 +0300

    Type room added new fields join, leave channels and clients map. Also added run method.

--- room.go
+++ room.go
@@ -4,4 +4,32 @@ type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clients.
 	forward chan []byte
+
+	// join is a channel for clients wishing to join the room.
+	join chan *client
+
+	// leave is a channel for clients wishing to leave the room.
+	leave chan *client
+
+	// clients holds all current clients in this room.
+	clients map[*client]bool
+}
+
+func (r *room) run()  {
+	for  {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
 }
