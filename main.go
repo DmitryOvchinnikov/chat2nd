ec8f4b5e0d0bc2a4f6479d7391034743215e8e66
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-10-05 17:25:51 +0300

    Type templateHandler added with ServeHTTP method.

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+// templ represents a single template
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Let's chat!
-				</body>
-			</html>
-		`))
-	})
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
