73ea11d9ddbbae08fa21fb60959ac40e5cfd49d4
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-10-15 22:30:58 +0300

    The auth service and the avatar_s implementation.

---
+++ avatar_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestAuthAvatar(t *testing.T) {
+	var authAvatar AuthAvatar
+
+	client := new(client)
+
+	url, err := authAvatar.GetAvatarURL(client)
+	if err != ErrNoAvatarURL {
+		t.Error("AuthAvatar.GetAvatarURL should return ErrNoAvatarURL when no value present")
+	}
+
+	// set a value
+	testURL := "http://url-to-gravatar/"
+	client.userData = map[string]interface{}{"avatar_url": testURL}
+	url, err = authAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("AuthAvatar.GetAvatarURL should return no error when value present")
+	}
+	if url != testURL {
+		t.Error("AuthAvatar.GetAvatarURL should return correct URL")
+	}
+
+}
