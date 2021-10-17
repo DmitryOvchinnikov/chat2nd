c79592c5d41028bba0aa37f4f0d33fd4f47fdd0a
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-10-17 18:01:22 +0300

    Fixing the existing implementations.

--- avatar.go
+++ avatar.go
@@ -20,55 +20,43 @@ type Avatar interface {
 	GetAvatarURL(ChatUser) (string, error)
 }
 
-type AuthAvatar struct {}
+type AuthAvatar struct{}
 
 var UseAuthAvatar AuthAvatar
 
-func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
-	if url, ok := c.userData["avatar_url"]; ok {
-		if urlStr, ok := url.(string); ok {
-			return urlStr, nil
-		}
+func (AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	url := u.AvatarURL()
+	if len(url) == 0 {
+		return "", ErrNoAvatarURL
 	}
 
-	return "", ErrNoAvatarURL
+	return url, nil
 }
 
-type GravatarAvatar struct {}
+type GravatarAvatar struct{}
 
 var UseGravatar GravatarAvatar
 
-func (GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if userID, ok := c.userData["userid"]; ok {
-		if userIDStr, ok := userID.(string); ok {
-			return "//www.gravatar.com/avatar/" + userIDStr, nil
-		}
-	}
-
-	return "", ErrNoAvatarURL
+func (GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	return "//www.gravatar.com/avatar/" + u.UniqueID(), nil
 }
 
-type FileSystemAvatar struct {}
+type FileSystemAvatar struct{}
 
 var UseFileSystemAvatar FileSystemAvatar
 
-func (FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
-	if userID, ok := c.userData["userid"]; ok {
-		if userIDStr, ok := userID.(string); ok {
-			files, err := ioutil.ReadDir("avatars")
-			if err != nil {
-				return "", ErrNoAvatarURL
-			}
+func (FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	if files, err := ioutil.ReadDir("avatars"); err == nil {
 
-			for _, file := range files {
-				if file.IsDir() {
-					continue
-				}
-				if match, _ := path.Match(userIDStr + "*", file.Name()); match {
-					return "/avatars/" + file.Name(), nil
-				}
+		for _, file := range files {
+			if file.IsDir() {
+				continue
+			}
+			if match, _ := path.Match(u.UniqueID()+"*", file.Name()); match {
+				return "/avatars/" + file.Name(), nil
 			}
 		}
+
 	}
 
 	return "", ErrNoAvatarURL
